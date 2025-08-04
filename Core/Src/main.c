394b4959e007e2db89e8ec0942b35266ce997a64
nakanishishuntaro <148457745+NakanishiShuntaro@users.noreply.github.com>
2025-08-04 20:09:38 +0900

    succeed test with main

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -85,6 +85,7 @@ static SensorData_t data_buffer;
 static volatile uint32_t data_buffer_index = 0;
 static volatile bool save_data_flag = false;
 static volatile bool read_sensor_flag = false;
+bool servo_init_flag_pre = false; // ソレノイド初期化フラグ
 bool servo_init_flag = false;
 /* USER CODE END PV */
 
@@ -108,14 +109,12 @@ void system_init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-
-
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -152,15 +151,11 @@ int main(void)
   MX_USART1_UART_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-  //system_test();
-  // タイマー割り込み開始
-  
+  // system_test();
+  //  タイマー割り込み開始
+
   solenoid_close();
 
-  servo_state.valve_operation_active = false;
-  servo_state.valve_operation_start_time = 0;
-  solenoid_state.solenoid_operation_active = false;
-  solenoid_state.solenoid_operation_start_time = 0;
   HAL_TIM_Base_Start_IT(&htim2);
   HAL_UART_Receive_IT(&huart1, &cmd, 1);
   /* USER CODE END 2 */
@@ -169,15 +164,16 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
     uint32_t current_time = HAL_GetTick();
-    if (servo_init_flag == true)
+    if (servo_init_flag == true && servo_init_flag_pre == false)
     {
       servo_init();
       servo_init_flag = false;
     }
+    servo_init_flag_pre = servo_init_flag;
     if (solenoid_state.solenoid_operation_active)
     {
-      // ソレノイド操作がアクティブな場合、開始時間が設定されていないならば現在時刻を設定
       if (solenoid_state.solenoid_operation_start_time == 0)
       {
         solenoid_state.solenoid_operation_start_time = current_time;
@@ -206,40 +202,40 @@ int main(void)
         servo_state.valve_operation_start_time = 0;
       }
     }
-        if (read_sensor_flag)
-        {
-          read_sensor_flag = false;
+    // if (read_sensor_flag)
+    // {
+    //   read_sensor_flag = false;
 
-          if (data_buffer_index < MAX_DATA_POINTS)
-          {
-            float temperature = MAX31855_Read_Temp(&hspi2);
-            float pressure = MCP3425_Read_Pressure(&hi2c1);
+    //   if (data_buffer_index < MAX_DATA_POINTS)
+    //   {
+    //     float temperature = MAX31855_Read_Temp(&hspi2);
+    //     float pressure = MCP3425_Read_Pressure(&hi2c1);
 
-            data_buffer = (SensorData_t){
-                .temp_data = temperature,
-                .press_data = pressure,
-                .is_nos_open = solenoid_state.solenoid_operation_active};
+    //     data_buffer = (SensorData_t){
+    //         .temp_data = temperature,
+    //         .press_data = pressure,
+    //         .is_nos_open = solenoid_state.solenoid_operation_active};
 
-            data_buffer_index++;
+    //     data_buffer_index++;
 
-            // バッファ満杯時はSD保存フラグを立てる
-            if (data_buffer_index >= MAX_DATA_POINTS)
-            {
-              save_data_flag = true;
-            }
-          }
-        }
+    //     // バッファ満杯時はSD保存フラグを立てる
+    //     if (data_buffer_index >= MAX_DATA_POINTS)
+    //     {
+    //       save_data_flag = true;
+    //     }
+    //   }
+    // }
 
-        // SD保存処理（非同期実行）
-        if (save_data_flag)
-        {
-          save_data_flag = false;
-          __disable_irq();
-          data_buffer_index = 0;
-          __enable_irq();
-          sd_save_data(&data_buffer);
-        }
-    
+    // // SD保存処理（非同期実行）
+    // if (save_data_flag)
+    // {
+    //   save_data_flag = false;
+    //   __disable_irq();
+    //   data_buffer_index = 0;
+    //   __enable_irq();
+    //   sd_save_data(&data_buffer);
+    // }
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -248,22 +244,22 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -274,9 +270,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -289,10 +284,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief I2C1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_I2C1_Init(void)
 {
 
@@ -319,14 +314,13 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
-
 }
 
 /**
-  * @brief SPI1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief SPI1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_SPI1_Init(void)
 {
 
@@ -357,14 +351,13 @@ static void MX_SPI1_Init(void)
   /* USER CODE BEGIN SPI1_Init 2 */
 
   /* USER CODE END SPI1_Init 2 */
-
 }
 
 /**
-  * @brief SPI2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief SPI2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_SPI2_Init(void)
 {
 
@@ -395,14 +388,13 @@ static void MX_SPI2_Init(void)
   /* USER CODE BEGIN SPI2_Init 2 */
 
   /* USER CODE END SPI2_Init 2 */
-
 }
 
 /**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM2_Init(void)
 {
 
@@ -440,14 +432,13 @@ static void MX_TIM2_Init(void)
   /* USER CODE BEGIN TIM2_Init 2 */
 
   /* USER CODE END TIM2_Init 2 */
-
 }
 
 /**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM3 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM3_Init(void)
 {
 
@@ -489,14 +480,13 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 2 */
   HAL_TIM_MspPostInit(&htim3);
-
 }
 
 /**
-  * @brief UART5 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief UART5 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_UART5_Init(void)
 {
 
@@ -522,14 +512,13 @@ static void MX_UART5_Init(void)
   /* USER CODE BEGIN UART5_Init 2 */
 
   /* USER CODE END UART5_Init 2 */
-
 }
 
 /**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART1_UART_Init(void)
 {
 
@@ -555,14 +544,13 @@ static void MX_USART1_UART_Init(void)
   /* USER CODE BEGIN USART1_Init 2 */
 
   /* USER CODE END USART1_Init 2 */
-
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -588,12 +576,11 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
-  * Enable DMA controller clock
-  */
+ * Enable DMA controller clock
+ */
 static void MX_DMA_Init(void)
 {
 
@@ -607,14 +594,13 @@ static void MX_DMA_Init(void)
   /* DMA1_Stream3_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Stream3_IRQn, 1, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream3_IRQn);
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -641,7 +627,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET);
 
   /*Configure GPIO pins : PA4 PA11 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_11;
+  GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_11;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -680,6 +666,7 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
     // 受信したデータで直接バルブ制御
     if (cmd == 'L')
     {
+
       servo_state.valve_operation_active = true;
     }
     else if (cmd == 'E')
@@ -767,9 +754,9 @@ int _write(int file, char *ptr, int len)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -782,12 +769,12 @@ void Error_Handler(void)
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
